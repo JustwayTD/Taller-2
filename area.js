be73395a1eaa762193f444fa0bed5d0b160d70e7
Jose L Gonzalez <jl_gonzal_02@hotmail.com>
2019-03-15 21:32:32 -0500

    creacion del js con la funncion del area

---
+++ area.js
@@ -0,0 +1,10 @@
+var calcular=function (radio) {
+  formulario = documment.getElementById()
+if (radio>0) {
+  area = 3.14*(radio^2);
+    alert("El area es igual a ", area);
+    else {
+      alert("no es calculable");
+    }
+    }
+}
