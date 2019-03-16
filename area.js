314593d4dc23a9b7f2828754743247906458aec0
Jose L Gonzalez <jl_gonzal_02@hotmail.com>
2019-03-15 22:09:32 -0500

    js y creacion de formulario para area de circulo

--- area.js
+++ area.js
@@ -1,10 +1,8 @@
 var calcular=function (radio) {
-  formulario = documment.getElementById()
-if (radio>0) {
-  area = 3.14*(radio^2);
-    alert("El area es igual a ", area);
-    else {
-      alert("no es calculable");
-    }
-    }
+  formulario = documment.getElementById("calculo");
+
+  var area = 0;
+  area = 3.14*(radio)^2;
+
+  alert("El area del circulo es ", area);
 }
